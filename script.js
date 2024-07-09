adbe423531c960e5bb7a6ebb4a9fd8ce89d27030
Russellier <russellvap13@gmail.com>
2024-07-09 16:44:46 +0800

    feat: Create functions for basic math operations
    Create four functions. One for each: addition,
    subtraction, multiplication, division.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return 'Error'
+  return a / b;
+}
+
+let x = 65132312;
+let y = 9846351;
+
+console.log( add(x, y) );
+console.log( subtract(x, y) );
+console.log( multiply(x, y) );
+console.log( divide(x, y) );
