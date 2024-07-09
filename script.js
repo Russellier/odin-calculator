2586c627bfff75f74a88a2275ace39bb727b7211
Russellier <russellvap13@gmail.com>
2024-07-09 17:00:27 +0800

    feat: Create operate(function)
    This function takes in three arguments: two
    numbers as operands and one string as the
    operator.

--- script.js
+++ script.js
@@ -19,8 +19,24 @@ function divide(a, b) {
 
 let x = 65132312;
 let y = 9846351;
+let operator = 'divide';
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case 'add':
+      return add(a, b);
+    case 'subtract':
+      return subtract(a, b);
+    case 'multiply':
+      return multiply(a, b);
+    case 'divide':
+      return divide(a, b);
+  }
+}
 
 console.log( add(x, y) );
 console.log( subtract(x, y) );
 console.log( multiply(x, y) );
 console.log( divide(x, y) );
+
+console.log( operate(x, y, operator) );
