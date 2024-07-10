d831e8dbd6b1b1b0bebce0fac77f26f80171c306
Russellier <russellvap13@gmail.com>
2024-07-10 11:59:39 +0800

    feat: Display number when clicked

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 "use strict";
 
+const displayWindow = document.querySelector('p.display');
+const numberBtns = document.querySelectorAll('.number');
+const operatorBtns = document.querySelectorAll('.operator');
+const equalSignBtn = document.querySelector('.equal-sign');
+
+let x;
+let y;
+let answer;
+let operator = '';
+let isOperatorClicked = false;
+
 function add(a, b) {
-  return a + b;
+  return answer = a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+  return answer = a - b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return answer = a * b;
 }
 
 function divide(a, b) {
-  if (b === 0) return 'Error'
-  return a / b;
+  if (b === 0) return answer = 'Error'
+  return answer = a / b;
 }
-
-let x = 65132312;
-let y = 9846351;
-let operator = 'divide';
 
 function operate(a, b, operator) {
   switch (operator) {
@@ -34,9 +41,25 @@ function operate(a, b, operator) {
   }
 }
 
-console.log( add(x, y) );
-console.log( subtract(x, y) );
-console.log( multiply(x, y) );
-console.log( divide(x, y) );
+// fix: user can input decimal point multiple times
+// fix: user can input 0 multiple times. e.g. '00000005'
+numberBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (isOperatorClicked) {
+      displayWindow.textContent = '';
+      isOperatorClicked = false;
+    }
+    displayWindow.textContent += button.innerText;
+  });
+});
 
-console.log( operate(x, y, operator) );
+operatorBtns.forEach((button) => {
+  button.addEventListener('click', () => {
+    isOperatorClicked = true;
+    operator = button.innerText;
+    if (!x) x = displayWindow.textContent;
+    else if (!y) y = displayWindow.textContent;
+    else operate(x, y, operator);
+    console.log(`${x} ${operator} ${y} : ${answer}`);
+  });
+});
